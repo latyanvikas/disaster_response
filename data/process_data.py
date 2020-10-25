3990b4dd7febea7577816f254e0813b65726aef3
latyanvikas <vikaslatyan@gmail.com>
2020-10-26 01:36:03 +0530

    initial commit

---
+++ data/process_data.py
@@ -0,0 +1,125 @@
+import sys
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def load_data(messages_filepath, categories_filepath):
+    """
+    load the disaster message and categories data 
+    
+    INPUT 
+    disaster_categories.csv
+    disaster_message.csv
+    
+    output 
+    dataframe - merged dataset from above two files
+    
+    """
+    
+    df_messages   = pd.read_csv(messages_filepath)
+    df_categories = pd.read_csv(categories_filepath)
+    df = pd.merge(df_messages,df_categories,on='id')
+    
+    return df
+
+
+def clean_data(df):
+    
+    """
+    claen the categores dataset
+    
+    inout 
+    categories data
+    
+    output 
+    
+    cleaned dataset
+    """
+    # create a dataframe for all the individual category columns
+    categories = df['categories'].str.split(pat=';', expand=True)
+    
+    # getting the first row of the categories dataframe
+    row = categories.iloc[0,:]
+    
+    # form the above list create the list of new columns
+    # we will be using lambda function to apply the name for all the columns
+    
+    cat_colnames = row.apply(lambda x:x[:-2])
+    
+    # rename the columns of `categories`
+    categories.columns = cat_colnames
+    
+    # set each value to be the last character of the string
+    # convert column from string to numeric
+
+    for column in categories:
+        categories[column] = categories[column].str[-1]
+        categories[column] = categories[column].astype(int)
+        
+    # drop the original categories column from the dataframe df
+    df=df.drop('categories',axis=1)
+    
+    # concatenate the original dataframe with the new `categories` dataframe created above
+    df = pd.concat([df,categories],axis=1)
+    
+    # remove duplicates 
+    df = df.drop_duplicates()
+    
+    return df
+
+        
+    
+
+def save_data(df, database_filename):
+    '''
+    save the final dataframe to database
+    
+    arguments:
+    
+    df - input dataframe
+    database_filename - database location
+   
+    '''
+    engine = create_engine('sqlite:///'+ database_filename)
+    df.to_sql('vk_disasterResponse', engine, index=False,if_exists='replace')  
+
+
+def main():
+    
+    """
+    Main function to perform the ETL taks
+    1) it will will extract the data from csv files
+    2) it will do the necessary transofrmation
+    3) it will load the data into sqlite database
+    
+    this function will not return anyrhing
+    
+    """
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
---
+++ models/train_classifier.py
@@ -0,0 +1,213 @@
+# import ntk libraries
+import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
+
+# import libraries
+import numpy as np
+import pandas as pd
+
+import sys
+import os
+import re
+from sqlalchemy import create_engine
+import pickle
+
+from scipy.stats import gmean
+# import relevant modules from sklearn
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import fbeta_score, make_scorer
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier, AdaBoostClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.base import BaseEstimator,TransformerMixin
+
+
+def load_data(database_filepath):
+    """
+    load the data we have prepared in ETL pipeline
+    
+    INPUT : database location 
+    OUTPTU : 
+    Output
+        X : message column
+        Y : dataframe with all categories
+        category_names : List of categories name
+    
+    """
+    
+    # loading data from database
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('vk_disasterResponse',engine)
+    
+    # As per our analysis in notebook, we know child_alone doesn't have any information, lets drop it
+    df=df.drop(['child_alone'],axis=1)
+    
+    # related columns doesn't have valid response, lets replace 2 with 1 as majority of the rows has this value. It mightbe due to error
+    df.loc[:,'related'] = df['related'].replace(2,1)
+    
+    
+    X = df['message']
+    Y = df.iloc[:,4:]
+    
+    # This will be used in classification_report
+    category_names = Y.columns 
+    return X, Y, category_names
+    
+
+def tokenize(text):
+    """
+    tokenize the text function
+    
+    input : message text
+    output : returns the clean tokenize words form clean text
+    """
+    
+    # replacing the urls text
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    
+    # find all URLs from given message
+    detected_urls = re.findall(url_regex, text)
+    
+    # replacing the URLs with strings in URL place holder
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+        
+    # Extract the word tokens from the message
+    tokens = nltk.word_tokenize(text)
+    
+    #lemmmatizing the tokenize words
+    lemmatizer = nltk.WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+    
+    return clean_tokens
+
+# building a verb extractor function
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+    
+    """
+    This function will rxtract the part of verb and will create new feature for ML classifier
+    """
+
+    def starting_verb(self, text):
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            first_word, first_tag = pos_tags[0]
+            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                return True
+        return False
+    
+    # Given it is a tranformer we can return the self 
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
+
+
+def build_model():
+    """
+    This will build the model pipeline
+    
+    Returns:
+    ML pipeline which will process the input message and apply a classifier
+    
+    
+    """
+    pipeline = Pipeline([
+        ('features', FeatureUnion([
+
+            ('text_pipeline', Pipeline([
+                ('count_vectorizer', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf_transformer', TfidfTransformer())
+            ])),
+
+            ('starting_verb', StartingVerbExtractor())
+        ])),
+
+        ('classifier', MultiOutputClassifier(AdaBoostClassifier()))
+    ])
+    
+    return pipeline
+        
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    
+    ''' 
+    
+    Evaluate the model performance by f1 score, precision and recall
+    Parameters:
+    model: a ML model
+    X_test: message from test set
+    Y_test: category value from test set
+    category_names: the names of the categories
+    Return:
+    None
+    '''
+
+    y_pred = model.predict(X_test)
+    Y_pred = pd.DataFrame(data=y_pred, 
+                          index=Y_test.index, 
+                          columns=category_names)
+    print(classification_report(Y_test, Y_pred, target_names=category_names))
+
+
+def save_model(model, model_filepath):
+    
+    '''
+    
+    This will save the model on a given location
+    
+    Parameters:
+    model: ML model 
+    model_filepath: location where model will be saved
+    Returns:
+    None
+    '''
+    pickle.dump(model, open(model_filepath, 'wb'))
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
